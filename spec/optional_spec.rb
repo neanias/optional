998d85b5027d20b6e89d9746ed1cde0222cb06cf
Will Mathewson <william.mathewson@freeagent.com>
2022-06-01 16:26:32 +0100

    Initial commit

---
+++ spec/optional_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Optional do
+  it "has a version number" do
+    expect(Optional::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
